2fac4a012d4377fee6e05a5682b1901d2ed14553
Chendi YU <cyuaf@ust.hk>
2016-03-24 11:43:47 +0800

    reorganize

--- src/main.c
+++ src/main.c
@@ -35,31 +35,12 @@
 GPIO_InitTypeDef GPIO_InitStructure;
 ADC_InitTypeDef ADC_InitStructure;
 ADC_CommonInitTypeDef ADC_CommonInitStructure;
+uint16_t CPAD_threshold;
 
-int
-main(int argc, char* argv[])
-{
-	SystemInit();
-	delay_init(168);
-	LCD_Init();
-	LCD_ShowString(30,40,200,16,16,"Hello World");
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOF, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC3, ENABLE);
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Low_Speed;
-	GPIO_Init(GPIOF, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Low_Speed;
-	GPIO_Init(GPIOF, &GPIO_InitStructure);
-
-
-	GPIO_WriteBit(GPIOF, GPIO_Pin_10, 0);
-	GPIO_WriteBit(GPIOF, GPIO_Pin_9, 1);
-
+/**
+ * initialize capacitive touch pad. use PF3.
+ */
+void CPADInit(){
 	ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
 	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
 	ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div4;
@@ -75,6 +56,87 @@ main(int argc, char* argv[])
 	ADC_Init(ADC3, &ADC_InitStructure);
 	ADC_Cmd(ADC3, ENABLE);
 	ADC_RegularChannelConfig(ADC3, ADC_Channel_9, 1, ADC_SampleTime_480Cycles);
+}
+
+/**
+ * read the touch pad adc value 10 times. use average as threshold
+ */
+void CPADCalibrate(){
+	uint8_t i;
+	uint16_t sum = 0;
+	for(i = 0; i < 10; i++){
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+   	    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+		GPIO_Init(GPIOF, &GPIO_InitStructure);
+		GPIO_ResetBits(GPIOF, GPIO_Pin_3);
+		delay_us(20);
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+		GPIO_Init(GPIOF, &GPIO_InitStructure);
+		delay_us(80);
+		ADC_SoftwareStartConv(ADC3);
+		while(!ADC_GetFlagStatus(ADC3, ADC_FLAG_EOC));
+		sum += ADC_GetConversionValue(ADC3);
+	}
+	CPAD_threshold = sum / 10 - 1000;
+}
+/**
+ * read touch pad value
+ */
+uint16_t getCPADValue(){
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_Init(GPIOF, &GPIO_InitStructure);
+	GPIO_ResetBits(GPIOF, GPIO_Pin_3);
+	delay_us(20);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOF, &GPIO_InitStructure);
+	delay_us(80);
+	ADC_SoftwareStartConv(ADC3);
+	while(!ADC_GetFlagStatus(ADC3, ADC_FLAG_EOC));
+	uint16_t result = ADC_GetConversionValue(ADC3);
+	LCD_ShowNum(30,80,result,4,16);
+	return result;
+}
+int
+main(int argc, char* argv[])
+{
+	SystemInit();
+	delay_init(168);
+	LCD_Init();
+	LCD_ShowString(30,40,200,16,16,"Hello World");
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOF, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC3, ENABLE);
+	CPADInit();
+	CPADCalibrate();
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Low_Speed;
+	GPIO_Init(GPIOF, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Low_Speed;
+	GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+
+	GPIO_WriteBit(GPIOF, GPIO_Pin_10, 0);
+	GPIO_WriteBit(GPIOF, GPIO_Pin_9, 1);
+
   // At this stage the system clock should have already been configured
   // at high speed.
 
@@ -82,32 +144,12 @@ main(int argc, char* argv[])
   // Infinite loop
   while (1)
     {
-//	  GPIOF->ODR ^= 3<<9;
-//	  delay_ms(500);
-//	  GPIO_WriteBit(GPIOC, GPIO_Pin_2, 0);
-//	  delay_us(100);
-//	  GPIO_WriteBit(GPIOC, GPIO_Pin_2, 1);
-//	  delay_us(100);
-	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-	  GPIO_Init(GPIOF, &GPIO_InitStructure);
-	  GPIO_ResetBits(GPIOF, GPIO_Pin_3);
-	  delay_ms(5);
-	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-	  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	  GPIO_Init(GPIOF, &GPIO_InitStructure);
-	  delay_us(80);
-	  ADC_SoftwareStartConv(ADC3);
-	  while(!ADC_GetFlagStatus(ADC3, ADC_FLAG_EOC));
-	  uint16_t result = ADC_GetConversionValue(ADC3);
-	  LCD_ShowNum(30,80,result,4,16);
-	  while(1);
+	  if(getCPADValue() > CPAD_threshold){
+		  GPIO_WriteBit(GPIOF, GPIO_Pin_9, 1);
+	  }
+	  else{
+		  GPIO_WriteBit(GPIOF, GPIO_Pin_9, 0);
+	  }
     }
 }
 #pragma GCC diagnostic pop
