2afecb8b84ff8ce47306b6dfcb98920c19359450
Chendi YU <cyuaf@ust.hk>
2016-04-06 16:19:57 +0800

    FATFS: able to read file name

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "diag/Trace.h"
 #include "stm32f4xx.h"
 #include "delay.h"
 #include "lcd.h"
 #include "CKey.h"
 #include "SDCard.h"
+#include "fatfs/ff.h"
 // ----------------------------------------------------------------------------
 //
 // Standalone STM32F4 empty sample (trace via DEBUG).
@@ -36,6 +38,61 @@
 static GPIO_InitTypeDef GPIO_InitStructure;
 CKey* keys[24];
 int i = 0;
+
+u8 exf_getfree(char *drv,u32 *total,u32 *free)
+{
+	FATFS *fs1;
+	u8 res;
+    u32 fre_clust=0, fre_sect=0, tot_sect=0;
+    //得到磁盘信息及空闲簇数量
+    res =(u32)f_getfree((const TCHAR*)drv, (DWORD*)&fre_clust, &fs1);
+    if(res==0)
+	{
+	    tot_sect=(fs1->n_fatent-2)*fs1->csize;	//得到总扇区数
+	    fre_sect=fre_clust*fs1->csize;			//得到空闲扇区数
+#if _MAX_SS!=512				  				//扇区大小不是512字节,则转换为512字节
+		tot_sect*=fs1->ssize/512;
+		fre_sect*=fs1->ssize/512;
+#endif
+		*total=tot_sect>>1;	//单位为KB
+		*free=fre_sect>>1;	//单位为KB
+ 	}
+	return res;
+}
+
+u8 mf_scan_files(char* path)
+{
+	FRESULT res;
+	FILINFO fno;
+	DIR dir;
+	u16 i;
+    char *fn;   /* This function is assuming non-Unicode cfg. */
+#if _USE_LFN
+ 	fileinfo.lfsize = _MAX_LFN * 2 + 1;
+	fileinfo.lfname = mymalloc(SRAMIN,fileinfo.lfsize);
+#endif
+
+    res = f_opendir(&dir,(const TCHAR*)path); //打开一个目录
+    if (res == FR_OK)
+	{
+    	i = strlen(path);
+		while(1)
+		{
+	        res = f_readdir(&dir, &fno);                   //读取目录下的一个文件
+	        if (res != FR_OK || fno.fname[0] == 0) break;  //错误了/到末尾了,退出
+	        //if (fileinfo.fname[0] == '.') continue;             //忽略上级目录
+#if _USE_LFN
+        	fn = *fileinfo.lfname ? fileinfo.lfname : fileinfo.fname;
+#else
+        	fn = fno.fname;
+#endif	                                              /* It is a file. */
+			LCD_ShowString(30, 250, 200, 16, 16, path);//打印路径
+			LCD_ShowString(30, 250, 250, 16, 16, fn);//打印文件名
+		}
+    }
+    f_closedir(&dir);
+    return res;
+}
 
 int
 main(int argc, char* argv[])
@@ -83,13 +140,27 @@ main(int argc, char* argv[])
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	while(SD_Init()){
 	}
-	LCD_ShowString(30,150,200,16,16,"SD Card OK    ");
-	LCD_ShowString(30,170,200,16,16,"SD Card Size:     MB");
-	LCD_ShowNum(30+13*8,170,SDCardInfo.CardCapacity>>20,5,16);//显示SD卡容量
-	if(SD_ReadDisk(buf,0,1)==0)	//读取0扇区的内容
-				{
-		for(sd_size=0;sd_size<512;sd_size++)LCD_ShowNum(30+40*(sd_size%20),190+sd_size/20*20,buf[sd_size],3,16);
-				}
+//	LCD_ShowString(30,150,200,16,16,"SD Card OK    ");
+//	LCD_ShowString(30,170,200,16,16,"SD Card Size:     MB");
+//	LCD_ShowNum(30+13*8,170,SDCardInfo.CardCapacity>>20,5,16);//显示SD卡容量
+//	if(SD_ReadDisk(buf,0,1)==0){	//读取0扇区的内容
+//		for(sd_size=0;sd_size<512;sd_size++){
+//			LCD_ShowNum(30+40*(sd_size%20),190+sd_size/20*20,buf[sd_size],3,16);
+//		}
+//	}
+	FATFS* fs = new FATFS;
+	FIL* file = new FIL;
+	FIL* ftemp = new FIL;
+	u8* fatbuf = new u8[512];
+	u32 total, free;
+	f_mount(fs,"0:",1);
+	exf_getfree("0", &total, &free);
+	LCD_ShowString(30,150,200,16,16,"FATFS OK!");
+	LCD_ShowString(30,170,200,16,16,"SD Total Size:     MB");
+	LCD_ShowString(30,190,200,16,16,"SD  Free Size:     MB");
+	LCD_ShowNum(30+8*14,170,total>>10,5,16);				//显示SD卡总容量 MB
+	LCD_ShowNum(30+8*14,190,free>>10,5,16);					//显示SD卡剩余容量 MB
+	mf_scan_files("0:");
 
   // At this stage the system clock should have already been configured
   // at high speed.
