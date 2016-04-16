55a6595eed4c549a09f3621030e1f585df436457
woaitianjin <dyy_1996@sjtu.edu.cn>
2016-04-16 17:29:41 +0800

    Added files via upload

---
+++ sound.h
@@ -0,0 +1,17 @@
+/*
+ * sound.h
+ *
+ *  Created on: 2016Äê4ÔÂ9ÈÕ
+ *      Author: eric
+ */
+
+#ifndef SOUND_H_
+#define SOUND_H_
+
+void Buf_Clear(int note);
+void Buf_Init();
+uint16_t Next_Round();
+
+//extern int buf_inuse = 0;
+
+#endif /* SOUND_H_ */
